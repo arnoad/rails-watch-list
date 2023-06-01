c80f1d3053eb38cdcf8c39e3d38c819a8da84438
arno arend <arnoarend@icloud.com>
2023-06-01 16:33:37 +0200

    finish backend

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   validates :movie, :list, presence: true
   validates :movie, uniqueness: { scope: :list }
+  validates :comment, length: { minimum: 6 }
 
   belongs_to :movie
   belongs_to :list
